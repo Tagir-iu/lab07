709940c4dc5cd6b32923be84436964e9c369714a
Tagir-iu <taga07taga@gmail.com>
2026-06-10 00:54:55 +0300

    Fix: remove cmake artifacts and update .gitignore

--- _builds/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/macbook/Desktop/lab07
-# Build directory: /Users/macbook/Desktop/lab07/_builds
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test1 "/Users/macbook/Desktop/lab07/_builds/test1")
-set_tests_properties(test1 PROPERTIES  _BACKTRACE_TRIPLES "/Users/macbook/Desktop/lab07/CMakeLists.txt;30;add_test;/Users/macbook/Desktop/lab07/CMakeLists.txt;0;")
-subdirs("third-party/gtest")
